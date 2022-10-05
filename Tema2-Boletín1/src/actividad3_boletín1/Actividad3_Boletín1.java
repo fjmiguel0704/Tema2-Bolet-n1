eff44f7125267dfd531bee41ebccf34b3f9fdd96
Fernando <Fernando@DESKTOP-3EE8PP0>
2022-10-05 19:21:10 +0200

    Tema2-Boletín1

--- Tema2-Boletín1/src/actividad3_boletín1/Actividad3_Boletín1.java
+++ Tema2-Boletín1/src/actividad3_boletín1/Actividad3_Boletín1.java
@@ -5,18 +5,35 @@ import java.util.Scanner;
 public class Actividad3_Boletín1 {
 
 	public static void main(String[] args) {
+		
+		//Creo la variable num tipo float. Ya que vamos a solicitar un número con décimales
 		float num;
+		
+		//Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
 		Scanner read = new Scanner (System.in);
+		
+		//Le pedimos al usuario que introduzca un número
 		System.out.print("Introduce un número: ");
+		
+		//Declaramos la variable num, que esta recogerá el dato introducido por el usuario
 		num = read.nextFloat();
 		
+		//Indicamos mediante condicionales, que si el valor de la variable num es mayor que -1, menor que 1 y distinto a 0
 		if (num > -1 && num <1 && num != 0) {
+			
+			//Le muestre al usuario por consola "casi-cero"
 			System.out.print("casi-cero");
 		}
 		
+		//Y en caso contrario, de ser el valor de la variable num = 1, -1 o 0 
 		else {
+			
+			//Le muestre al usuario que no es un número casi-cero
 			System.out.print("No es casi-cero");
 		}
+		
+		//Cerramos el Scanner
+		read.close();
 	
 	}
 
--- Tema2-Boletín1/src/actividad4_boletín1/Actividad4_Boletín1.java
+++ Tema2-Boletín1/src/actividad4_boletín1/Actividad4_Boletín1.java
@@ -5,21 +5,42 @@ import java.util.Scanner;
 public class Actividad4_Boletín1 {
 
 	public static void main(String[] args) {
+		
+		//Creo las variables num1 y num2, que contendrán los datos a ordenar de menor a mayor
 		int num1;
 		int num2;
+		
+		//Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
 		Scanner read = new Scanner (System.in);
+		
+		//Le pedimos al usuario que introduzca el primer número
 		System.out.print("Inroduce el primer número: ");
+		
+		//Declaramos la variable num1, que será el primer dato que ingrese el usuario
 		num1 = read.nextInt();
+		
+		//Le pedimos al usuario que introduzca el segundo número
 		System.out.print("Introduce el segundo número: ");
+		
+		//Declaramos la variable num2, que será el segundo dato que ingrese el usuario
 		num2 = read.nextInt();
 		
+		//Mediante condicionales, vamos a indicar, que si el valor de num1 es menor a num2
 		if (num1 < num2) {
+			
+			//Le muestre al usuario por consola, primero num1 (valor menor) y luego num2 (valor mayor)
 			System.out.print(num1 + ", " + num2);
 		}
 		
+		//Y en caso contrario, si es menor num2 a num1
 		else {
+			
+			//Le muestre al usuario, primero num2 y luego num1
 			System.out.print(num2 + ", " +num1);
 		}
+		
+		//Cerramos el Scanner
+		read.close();
 
 	}
 
