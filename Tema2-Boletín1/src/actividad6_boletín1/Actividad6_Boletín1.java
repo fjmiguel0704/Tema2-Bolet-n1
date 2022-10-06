d7a1b025c3bebd277b9ef9ada0af4603a687a355
fernando <fernando@DESKTOP-MKLFSRU.home>
2022-10-07 00:25:46 +0200

    Tema2-Boletín1

---
+++ Tema2-Boletín1/src/actividad6_boletín1/Actividad6_Boletín1.java
@@ -0,0 +1,41 @@
+package actividad6_boletín1;
+
+import java.util.Scanner;
+
+public class Actividad6_Boletín1 {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		int raizCuadrada;
+		int xPositive ;
+		int xNegative;
+		
+		Scanner read = new Scanner (System.in);
+		System.out.print("¿Cuánto vale a?");
+		a = read.nextInt();
+		System.out.print("¿Cuánto vale b ?");
+		b = read.nextInt();
+		System.out.print("¿Cuánto vale c?");
+		c = read.nextInt();
+		
+		
+		raizCuadrada = (int) (Math.pow(b, 2) -4 *a*c);
+			
+		if (a==0 || raizCuadrada <0) {
+			System.out.print("No hay solución");
+		}
+		
+		else {
+			xPositive = (int) ((-b + (Math.sqrt(raizCuadrada))) / (2*a));
+			xNegative = (int) ((-b - (Math.sqrt(raizCuadrada))) / (2*a));
+			System.out.println("Primera solución real: " + xPositive);
+			System.out.print("Segunda solución real: " + xNegative);
+		}
+			
+		
+		}
+	}
+
+
---
+++ Tema2-Boletín1/src/actividad7_boletín1/Actividad7_Boletín1.java
@@ -0,0 +1,10 @@
+package actividad7_boletín1;
+
+public class Actividad7_Boletín1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
