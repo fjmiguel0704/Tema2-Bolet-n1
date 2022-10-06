7a9c8a33faa46fc16b6cdbd67d3a12194dad38f9
fernando <fernando@DESKTOP-MKLFSRU.home>
2022-10-07 00:26:57 +0200

    Tema2-Boletín1

--- Tema2-Boletín1/src/actividad7_boletín1/Actividad7_Boletín1.java
+++ Tema2-Boletín1/src/actividad7_boletín1/Actividad7_Boletín1.java
@@ -1,9 +1,18 @@
 package actividad7_boletín1;
 
+import java.util.Scanner;
+
 public class Actividad7_Boletín1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num;
+		Scanner read = new Scanner (System.in);
+		System.out.print("Introduce el número: ");
+		num = read.nextInt();
+		
+		//if () {
+			
+		//}
 
 	}
 
