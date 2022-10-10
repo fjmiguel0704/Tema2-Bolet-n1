17ab0e417c2130ef4bf6531a415e6015da7f1ef2
fjmiguel <fjmiguel@106-16.info2>
2022-10-10 13:27:42 +0200

    Tema2-Boletín1

--- Tema2-Boletín1/src/actividad10_boletín1/Actividad10_Boletín1.java
+++ Tema2-Boletín1/src/actividad10_boletín1/Actividad10_Boletín1.java
@@ -5,37 +5,60 @@ import java.util.Scanner;
 public class Actividad10_Boletín1 {
 
 	public static void main(String[] args) {
+		
+		//Creo 3 variables, en las cuales, cada una recogerá un valor introducido por el usuario
 		int numero1;
         int numero2;
         int numero3;
+        
+        //Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
         Scanner read = new Scanner (System.in);
+        
+        //Le pedimos al usuario que introduzca un primer número
         System.out.print("Introduce el primer número:");
+        
+        //Declaramos la variable numero1, que recogerá el primer dato introducido por el usuario
         numero1 = read.nextInt();
+        
+        //Le pedimos al usuario que introduzca un segundo número
         System.out.print("Introduce el segundo número:");
+        
+        //Declaramos la variable numero2, que recogerá el segundo dato introducido por el usuario
         numero2 = read.nextInt();
+        
+        //Le pedimos al usuario que introduzca un tercer número
         System.out.print("Introduce el tercer número:");
+        
+        //Declaramos la variable numero3, que recogerá el tercer dato introducido por el usuario
         numero3 = read.nextInt();
         
+        //Mediante condicionales indicaremos, que si la suma de numero1 + numero2 es igual a numero3
         if (numero1 + numero2 == numero3) {
+        	
+        	//Me imprima por pantalla que esa suma es igual a numero3
             System.out.println("La suma de " +numero1 + " + " + numero2 + " es igual a " + numero3);
         }
-        else {
-            System.out.println("La suma de " +numero1 +" + " +numero2 + " no es igual a " + numero3);
-        }
         
-        if (numero1 + numero3 == numero2) {
-            System.out.println("La suma de " +numero1 + " + " + numero3 + " es igual a " + numero2);
-        }
-        else {
-            System.out.println("La suma de " +numero1 +" + " +numero3 + " no es igual a " + numero2);
-        }
-        if (numero2 + numero3 == numero1) {
+        //En caso contrario, si la suma de numero1 + numero3 es igual a numero2
+        else if (numero1 + numero3 == numero2) {
+        	
+        	//Me imprima por pantalla que esa suma es igual a numero2
+        	System.out.println("La suma de " +numero1 + " + " + numero3 + " es igual a " + numero2);
+        } 
+        
+        //En caso contrario, si la suma de numero2 + numero3 es igual a numero1
+        else if (numero2 + numero3 == numero1)  {
+        	
+        	//Me imprima por pantalla que esa suma es igual a numero1
             System.out.println("La suma de " +numero2 + " + " + numero3 + " es igual a " + numero1);
         }
-        else {
-            System.out.print("La suma de " +numero2 +" + " +numero3 + " no es igual a " + numero1);
-        }
         
+        //Y en el caso contrario de todo lo anterior, es decir, que no se cumpla ninguna de las condiciones, me imprima el siguiente mensaje
+        else {
+        	System.out.print("Ninguna de la suma de dos números es el resultado del otro número");
+        }
+       
+        //Cerramos el Scanner
         read.close();
 
 	}
--- Tema2-Boletín1/src/actividad9_boletín1/Actividad9_Boletín1.java
+++ Tema2-Boletín1/src/actividad9_boletín1/Actividad9_Boletín1.java
@@ -5,31 +5,52 @@ import java.util.Scanner;
 public class Actividad9_Boletín1 {
 
 	public static void main(String[] args) {
-		int piedra = 0;
-        int papel = 1;
-        int tijeras = 2;
+		
+		//Creo 3 variables constantes, piedra, papel y tijeras y les asigno a cada una un valor que nunca cambiará
+		final int piedra = 0;
+		final int papel = 1;
+		final int tijeras = 2;
+		
+		//Creo otras dos variables, jugador1 y jugador2, que recogerán el valor introducido por los usuarios 
         int jugador1;
         int jugador2;
+        
+        //Creamos el Scanner para leer por el teclado todo lo introducido por el usuario
         Scanner read = new Scanner (System.in);
+        
+        //Le pedimos al primer participante que introduzca, piedra, papel o tijeras. Teniendo en cuenta que piedra equivale a 0, papel a 1 y tijeras a 2
         System.out.print("¿Piedra = 0, Papel = 1 o Tijeras = 2?: ");
+        
+        //Declaramos la variable jugador1, que guardará el dato que introduzca el primer jugador. Bien será, 1, 2 o 3
         jugador1 = read.nextInt();
+        
+        //Le pedimos al segundo participante que introduzca, piedra, papel o tijeras. Teniendo en cuenta que piedra equivale a 0, papel a 1 y tijeras a 2
         System.out.print("¿Piedra = 0, Papel = 1 o Tijeras = 2?: ");
+        
+        //Declaramos la variable jugador2, que guardará el dato que introduzca el segundo jugador. Bien será, 1, 2 o 3
         jugador2 = read.nextInt();
-
-        if(jugador1==piedra && jugador2==papel) {
-            System.out.print("Papel gana a piedra. ¡Ganador Jugador 2!");
-        } else if(jugador1==piedra && jugador2==tijeras) {
-            System.out.print("Piedra gana a tijeras. ¡Ganador Jugador 1!");
-        } else if (jugador1==papel && jugador2==tijeras) {
-            System.out.print("Tijeras gana a Papel. ¡Ganador Jugador 2!");
-        } else if (jugador1==papel && jugador2==piedra) {
-            System.out.print("Papel gana a Piedra. ¡Ganador Jugador 1!");
-        } else if (jugador1==tijeras && jugador2==piedra) {
-            System.out.print("Piedra gana a Tijeras. ¡Ganador Jugador 2!");
-        } else if (jugador1==tijeras && jugador2==papel) {
-            System.out.print("Tijeras gana a Papel. ¡Ganador Jugador 1!");
+        
+        //Mediante condicionales, indicamos, que si el valor de jugador1 es igual al de jugador2
+        if(jugador1==jugador2) {
+        	
+        	//Me imprima por pantalla que ha habido un empate entre ambos participantes
+        	System.out.print("Hay Empate");
+        } 
+        
+        //En caso contrario, si jugador1 es igual a piedra, papel o tijeras y jugador 2 a tijeras, piedra o papel
+        else if (jugador1==piedra && jugador2==tijeras || jugador1==papel && jugador2==piedra || jugador1==tijeras && jugador2==papel) {
+        	
+        	//Me imprima que el ganador ha sido el Jugador 1
+        	System.out.print("¡Ganador Jugador1!");
         }
         
+        //Y en caso contrario, me imprima que el ganador ha sido el Jugador 2
+        else {
+        	System.out.print("¡Ganador Jugador2!");
+        }
+        
+        
+        //Cerramos el Scanner
         read.close();
 
 	}
